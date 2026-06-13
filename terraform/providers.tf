7c5f1bf6abcdf3d4729ba5d125649bc0ade94f86
Adel Alzoabi <adel.krut.qwerty@gmail.com>
2026-06-13 19:58:40 +0300

    init project

---
+++ terraform/providers.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    freeipa = {
+      version = "5.1.0"
+      source  = "rework-space-com/freeipa"
+    }
+  }
+}
+
+provider "freeipa" {
+  host     = var.freeipa_host
+  username = var.freeipa_username
+  password = var.freeipa_password
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,14 @@
+variable "freeipa_host" {
+  description = "FreeIPA host"
+  type        = string
+}
+
+variable "freeipa_username" {
+  description = "FreeIPA username"
+  type        = string
+}
+
+variable "freeipa_password" {
+  description = "FreeIPA password"
+  type        = string
+}
