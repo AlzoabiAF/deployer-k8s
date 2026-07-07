7dca6d965ed56ab489898e768eb12c692f6af630
Adel Alzoabi <adel.krut.qwerty@gmail.com>
2026-07-08 00:19:51 +0300

    add pre-hook

--- terraform/dns_record.tf
+++ terraform/dns_record.tf
@@ -1,47 +1,47 @@
 resource "freeipa_dns_record" "record_k8s_control_node_1" {
   zone_name = "alzoabi.lab"
-  name = "control-node-1"
-  type = "A"
-  records = [ "10.0.0.21" ]
-  ttl = 3600
+  name      = "control-node-1"
+  type      = "A"
+  records   = ["10.0.0.21"]
+  ttl       = 3600
 }
 
 resource "freeipa_dns_record" "record_k8s_control_node_2" {
   zone_name = "alzoabi.lab"
-  name = "control-node-2"
-  type = "A"
-  records = [ "10.0.0.22" ]
-  ttl = 3600
+  name      = "control-node-2"
+  type      = "A"
+  records   = ["10.0.0.22"]
+  ttl       = 3600
 }
 
 resource "freeipa_dns_record" "record_k8s_control_node_3" {
   zone_name = "alzoabi.lab"
-  name = "control-node-3"
-  type = "A"
-  records = [ "10.0.0.23" ]
-  ttl = 3600
+  name      = "control-node-3"
+  type      = "A"
+  records   = ["10.0.0.23"]
+  ttl       = 3600
 }
 
 resource "freeipa_dns_record" "record_k8s_worker_node_1" {
   zone_name = "alzoabi.lab"
-  name = "worker-node-1"
-  type = "A"
-  records = [ "10.0.0.31" ]
-  ttl = 3600
+  name      = "worker-node-1"
+  type      = "A"
+  records   = ["10.0.0.31"]
+  ttl       = 3600
 }
 
 resource "freeipa_dns_record" "record_k8s_worker_node_2" {
   zone_name = "alzoabi.lab"
-  name = "worker-node-2"
-  type = "A"
-  records = [ "10.0.0.32" ]
-  ttl = 3600
+  name      = "worker-node-2"
+  type      = "A"
+  records   = ["10.0.0.32"]
+  ttl       = 3600
 }
 
 resource "freeipa_dns_record" "record_k8s_gw_node" {
   zone_name = "alzoabi.lab"
-  name = "gw-1"
-  type = "A"
-  records = [ "10.0.0.1" ]
-  ttl = 3600
+  name      = "gw-1"
+  type    = "A"
+  records   = ["10.0.0.1"]
+  ttl       = 3600
 }
--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,4 +1,6 @@
 terraform {
+  required_version = ">= 1.0"
+
   required_providers {
     freeipa = {
       version = "5.1.0"
